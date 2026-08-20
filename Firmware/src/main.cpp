550e8de32d148d82ee27efa5cfd10a53e870f988
Miles <elijahtuinstra@gmail.com>
2026-08-19 18:53:18 -0600

    add suppourt for multiple buttons

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -11,12 +11,12 @@
 #define TFT_DC 8
 #define TFT_CS 7
 #define TFT_BL 6
-//#define BUZZER 5
-#define BUTTON1 0
-//#define BUTTON2 1
-//#define BUTTON3 2
-//#define BUTTON4 3
-//#define BUTTON5 4
+#define BUZZER 0
+#define BUTTON1 1
+#define BUTTON2 2
+#define BUTTON3 3
+#define BUTTON4 4
+#define BUTTON5 5
 
 bool pastBM1 = HIGH; //past button mode 1
 
@@ -56,6 +56,35 @@ void clockTick() {
   }
 }
 
+struct Button {
+    uint8_t pin;
+    bool lastState;
+    const char* name;
+};
+
+Button clockButtons[] = {
+    {BUTTON1, HIGH, "Button 1"},
+    {BUTTON2, HIGH, "Button 2"},
+    {BUTTON3, HIGH, "Button 3"},
+    {BUTTON4, HIGH, "Button 4"},
+    {BUTTON5, HIGH, "Button 5"}
+};
+
+const int NUM_BUTTONS = sizeof(clockButtons) / sizeof(clockButtons[0]);
+
+bool isButtonPress(Button& button) {
+    bool currentState = digitalRead(button.pin);
+    bool triggered = false;
+
+    if (currentState == LOW && button.lastState == HIGH) {
+        triggered = true;
+        delay(50);
+    }
+
+    button.lastState = currentState;
+    return triggered;
+}
+
 void startUpMessage() {
     tft.print("ouu shii");
 }
@@ -65,6 +94,24 @@ void clearScreen() {
     tft.setCursor(10,10);
 }
 
+void displayClock() {
+    if (clockMode == 0) {
+            tft.setTextSize(8);
+            if (lastMinute != minutes) { //this if loop lets the screen not try to reprint the time when it doesn't need to
+            clearScreen();
+            tft.printf("%02d:%02d\n", hours, minutes);
+            lastMinute = minutes;
+        }
+    } else if (clockMode == 1) {
+            tft.setTextSize(5);
+            if (lastSecond != seconds) { //this if loop lets the screen not try to reprint the time when it doesn't need to
+            clearScreen();
+            tft.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+            lastSecond = seconds;
+        }
+    }
+}
+
 void setup() {
     Serial.begin(115200);
     delay(1000);
@@ -72,8 +119,10 @@ void setup() {
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, LOW);
 
-    pinMode(BUTTON1, INPUT_PULLUP);
 
+    for (int i = 0; i < NUM_BUTTONS; i++) {
+        pinMode(clockButtons[i].pin, INPUT_PULLUP);
+    }
 
     tft.init(76, 284); //display size
     tft.setOffsets(82, 18);
@@ -91,28 +140,34 @@ void setup() {
 }
 
 void loop() {
-    if (clockMode == 0) {
-            tft.setTextSize(8);
-            if (lastMinute != minutes) { //this if loop lets the screen not try to reprint the time when it doesn't need to
+    //displayClock();
+
+    for (int i = 0; i < NUM_BUTTONS; i++) {
+        if (isButtonPress(clockButtons[i])) {
             clearScreen();
-            tft.printf("%02d:%02d\n", hours, minutes);
-            lastMinute = minutes;
-        }
-    } else if (clockMode == 1) {
-            tft.setTextSize(5);
-            if (lastSecond != seconds) { //this if loop lets the screen not try to reprint the time when it doesn't need to
-            clearScreen();
-            tft.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
-            lastSecond = seconds;
+            tft.setTextSize(3);            
+            tft.printf("%s was pressed!\n", clockButtons[i].name);
+            delay(50);
         }
     }
 
+    /*
     bool currentModeBtnState = digitalRead(BUTTON1);
     if (currentModeBtnState == LOW && pastBM1 == HIGH) {
-        
+        if (clockMode == 0) {
+            clearScreen();
+            tft.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+            tft.printf("%02d:%02d\n", hours, minutes);
+            clockMode = 1;
+        } else if (clockMode == 1) {
+            clearScreen();
+            tft.printf("%02d:%02d\n", hours, minutes);
+            clockMode = 0;
+        }
         Serial.println("Button was pressed!");
         delay(200);
     }
 
     pastBM1 = currentModeBtnState;
+    */
 }
